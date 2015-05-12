d292447d4cb81fe852b115d5795432726a263303
Umang Jain <umang94@gmail.com>
2015-05-12 10:46:03 +0530

    Minor changes to frontend code

--- frontend/server.js
+++ frontend/server.js
@@ -12,36 +12,28 @@ var app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-app.get("/", function(req,res){
-	//This is the function going to be executed when the browser requests a get on the
-	//homepage
-	
-	var html = '<form action="/" method="post">' +
-               'Enter the language: ' +
-               '<input type="text" name="language" placeholder="..." />' +
-               '<br>' +
-	       'Enter the repository name : ' +
-	       '<input type="text" name="repository" placeholder="..." />' + 
-               '<button type="submit">Submit</button>' +
-            '</form>';
-	res.send(html);
-});
+app.use(express.static(__dirname + "/public"));
 
 app.post('/', function(req, res){
-	var language = req.body.language;
-	var repository = req.body.repository;
+	var language = req.body.language,
+	    repository = req.body.repository;
+        console.log("language: " + language + ", repo: " + repository);
 	var python = require('child_process').spawn('python', 
 		// Array of parameters for the command python
-		["~/Desktop/MTP/Code/recommender.py",
+		["/Users/umang/Desktop/MTP/Code/recommender.py",
 		"-r", repository, "-l", language]
 		);
-	var output = "";
-	python.stdout.on('data', function(){console.log(data)});
-	console.log("Works");
-	console.log(output);
+	python.stdout.on('data', function(data) {
+		console.log('stdout: ' + data);
+		res.header('Content-Type', 'application/json');
+		res.send(200, data);
+	});
+	python.stderr.on('data', function(data) { console.log("stderr: " + data); });
+	/*
 	python.on('close', function(code){
+		console.log("child process exited with code: " + code);
 		if(code !== 0){return res.send(500,code);}
 		return res.send(200, output)
-	});
+	});*/
 });
 app.listen(8888);
